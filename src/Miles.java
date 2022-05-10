796b10d1b529d8e2da3ab463e06e3535300a004f
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2022-05-10 18:25:16 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,10 @@
+public class Miles {
+    public static void main(String[] args) {
+
+        int price = 2045;
+        int rublesForAMile = 20;
+
+        int miles = price / rublesForAMile;
+        System.out.println(miles);
+    }
+}
